4baab84fbcc3c3fa851c2d3622194999afcf0667
Emily Robinson <robinson.es@gmail.com>
2020-08-07 14:34:40 -0600

    remove failing tests

--- tests/testthat/test-geocode_tests.R
+++ tests/testthat/test-geocode_tests.R
@@ -1,9 +1,7 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
-  expect_equal(2 * 2, 10)
 })
 
 test_that("addition works", {
   expect_equal(2 + 4, 6)
-  expect_equal(2 + 5, 10)
 })
